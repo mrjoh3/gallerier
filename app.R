ea42cd56199f6f80a446e433428a8ddf7ec82933
Matt Johnson <mrjoh3@gmail.com>
2018-12-26 21:32:37 +1100

    minor cleanup

--- app.R
+++ app.R
@@ -2,22 +2,24 @@
 
 library(shiny)
 library(shinythemes)
+library(htmltools)
 library(dplyr)
+library(tidyr)
 library(hashids)
+library(lubridate)
 
 source('src/lightbox.R')
 source('src/photoswipe.R')
 
-images <<- data.frame(src = list.files('www/img')) %>%
+images <- data.frame(src = list.files('www/img')) %>%
   tidyr::separate(col = 'src', c('txt', 'date', 'time', 'msec'), sep = '_|\\.', remove = FALSE) %>%
   rowwise() %>%
   mutate(date = lubridate::ymd(date),
          key = hashids::encode(1e3 + as.integer(msec), hashid_settings(salt = 'this is my salt')))
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(theme = shinytheme("superhero"),
    
-   # Application title
     titlePanel(withTags(
       div("Shiny Image Galleries",
           div(class = 'pull-right',
@@ -44,19 +46,17 @@ ui <- fluidPage(theme = shinytheme("superhero"),
         )
       )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
    
   output$lb <- renderUI({
     
-    # gallery DIV
     lightbox_gallery(images[sample(1:nrow(images), 12, replace = FALSE),], 'gallery', display = TRUE)
     
   })
   
   output$ps <- renderUI({
     
-    # gallery DIV
     photoswipe_gallery(images[sample(1:nrow(images), 12, replace = FALSE),], 'ps-gallery', display = TRUE)
     
   })
