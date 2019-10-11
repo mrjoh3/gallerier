0d2074b7bcc083a48168aaffde98a808bcc88148
Matt Johnson <mrjoh3@gmail.com>
2019-10-11 11:15:21 +1100

    can change gallery css. libs copied from package if missing. add docs to lightbox

--- R/lightbox.R
+++ R/lightbox.R
@@ -1,10 +1,34 @@
 
-lightbox_gallery <- function(df, gallery, display = 'block'){
+
+#' @title Create Lightbox Gallery
+#' @param df data.frame containing
+#' @param gallery character label identifying gallery
+#' @param css character file path to css file. If missing default style is used
+#' @param display character 
+#'
+#' @importFrom shiny tags includeScript includeCSS
+#' @importFrom fs dir_copy
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+lightbox_gallery <- function(df, gallery, css, display = 'block'){
+  
+  dir.create('www')
+  
+  if (missing(css)) {
+    css <- file.path(system.file('css', package = 'gallerier'), 
+                    "styles.css")
+  }
+  if (!(dir.exists('www/lightbox-2.10.0'))) {
+    fs::dir_copy(system.file('js/lightbox-2.10.0', package = 'gallerier'), 
+                 'www/lightbox-2.10.0')
+  }
   
   tags$div(style = sprintf('display: %s;', display),
            tagList(tags$head(
-                     tags$link(rel = "stylesheet", type = "text/css", href = "lightbox-2.10.0/lightbox.min.css"),
-                     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+                     tags$link(rel = "stylesheet", type = "text/css", href = "lightbox-2.10.0/lightbox.min.css")
                    ),
                    tags$div(class = 'card-deck',
                             lapply(seq_len(nrow(df)), function(i){
@@ -12,7 +36,7 @@ lightbox_gallery <- function(df, gallery, display = 'block'){
                                        tags$a(id = df$key[i],
                                               href = paste0('img/', df$src[i]),
                                               `data-lightbox` = gallery, # this identifies gallery group
-                                              `data-title` = df$date[i],
+                                              `data-title` = df$date[i], # this is where complex title (glue) added
                                               tags$img(class = 'card-img-top',
                                                        src = paste0("img/", df$src[i]),
                                                        width = '80px',
@@ -20,7 +44,8 @@ lightbox_gallery <- function(df, gallery, display = 'block'){
                                        )
                             })
                    ),
-                   includeScript("www/lightbox-2.10.0/lightbox.min.js")
+                   includeScript("www/lightbox-2.10.0/lightbox.min.js"),
+                   includeCSS(css)
            ))
   
 }
